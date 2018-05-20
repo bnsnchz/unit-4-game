7fb7883d86f315ebb8e77dc521891db67258fb90
bnsnchz <bsanchez4488@gmail.com>
2018-05-19 21:52:40 -0700

    added html finished java

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,90 @@
-var targetNumber = Math.floor(Math.random()* (120 - 19)+19);
+$(document).ready(function() {
+var Random = Math.floor(Math.random()* (120 - 19)+19)
 
-$("#number-to-guess").text(targetNumber);
+$("#number-to-guess").text(Random);
 
-var crystals = $("#crystals");
 var counter = 0;
-var numberOptions = Array.from({length: 4}, () => Math.floor(Math.random() * 12)+1);
+var wins = 0;
+var loses = 0;
+var numberOption1 = Math.floor(Math.random() * 12)+1;
+var numberOption2 = Math.floor(Math.random() * 12)+1;
+var numberOption3 = Math.floor(Math.random() * 12)+1;
+var numberOption4 = Math.floor(Math.random() * 12)+1; 
 
-crystals.on("click", ".crystal-image", function() {
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    counter += crystalValue;
-    alert("New Score: " + counter);
-    if (counter === targetNumber){
-    alert("You Win!");
-    }
+$("numberWins").text(wins);
+$("numberLosses").text(loses);
 
-    else if (counter >= targetNumber) {
-    alert("You Lose!");
-    }
+function reset(){
+    targetNumber = Math.floor(Math.random()* (120 - 19)+19)
+    console.log(Random);
+    $("#number-to-guess").text(Random);
+    var numberOption1 = Math.floor(Math.random() * 12)+1;
+    var numberOption2 = Math.floor(Math.random() * 12)+1;
+    var numberOption3 = Math.floor(Math.random() * 12)+1;
+    var numberOption4 = Math.floor(Math.random() * 12)+1; 
+    counter= 0;
+    $("#counter").text(counter);
+    } 
+
+    function winner(){
+        alert("You won!");
+          wins++; 
+          $('#numberWins').text(wins);
+          reset();
+        }
        
+    function loser(){
+        alert ("You lose!");
+          losses++;
+          $('#numberLosses').text(losses);
+          reset();
+        }
 
 
-        
-})
+$("#crystal1").on ("click", function(){
+    counter = counter + numberOption1;
+    console.log("New Total= " + counter);
+    $("#total").text(counter); 
+          if (counter == Random){
+          winner();
+        }
+        else if ( counter > Random){
+          loser();
+        }   
+  })  
+  $("#crystal2").on ("click", function(){
+    counter = counter + numberOption2;
+    console.log("New Total= " + counter);
+    $("#total").text(counter); 
+        if (counter == Random){
+          winner();
+        }
+        else if ( counter > Random){
+          loser();
+        } 
+  })  
+  $("#crystal3").on ("click", function(){
+    counter = counter + numberOption3;
+    console.log("New Total= " + counter);
+    $("#total").text(counter);
+
+          if (counter == Random){
+          winner();
+        }
+        else if ( counter > Random){
+          loser();
+        } 
+  })  
+  $("#crystal4").on ("click", function(){
+    counter = counter + numberOption4;
+    console.log("New Total= " + counter);
+    $("#total").text(counter); 
+      
+          if (counter == Random){
+          winner();
+        }
+        else if ( counter > Random){
+          loser();
+        }
+  });   
+});
